dfdb841d14ca5849a7d0e1412807fd11e8de703f
Fredthedoggy <45927799+fredthedoggy@users.noreply.github.com>
2023-01-16 12:09:45 -0500

    Remove APNG Code (Comment it out)

--- cli/build.cjs
+++ cli/build.cjs
@@ -1,12 +1,19 @@
-const { exec } = require("child_process");
+const {exec} = require("child_process");
 const fs = require("fs");
 if (fs.existsSync('build')) {
-  fs.rmSync('build', {recursive: true});
+    fs.rmSync('build', {recursive: true});
 }
-exec('npx tsc -p tsconfig.json', (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log("Build Complete");
+exec('npx tsc -p tsconfig.json', (err, stdout, stderr) => {
+    if (stdout) {
+        console.log(stdout);
+    }
+    if (stderr) {
+        console.error(stderr);
+    }
+    if (err) {
+        console.error(err);
+        process.exit(1);
+        return;
+    }
+    console.log("Build Complete");
 });
